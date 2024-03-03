cb8954b5ca84b3ce08f04b2db541d6b324a26638
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2024-03-03 23:56:17 +0100

    Add global styles and routes set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import NotFound from "./pages/not-found.jsx";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+export default function Pages() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<App />} />
+        {/* the route below automatically routes "/" to "/home" page */}
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Pages />
+  </React.StrictMode>
+);

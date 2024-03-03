a16a4f9b3ec61b198f8a4e5ddf419cd347329e0c
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2024-03-04 00:46:31 +0100

    Add all the necessary pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import "./index.css";
 import NotFound from "./pages/not-found.jsx";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
+import Trending from "./pages/trending.jsx";
+import Popular from "./pages/popular.jsx";
+import Series from "./pages/series.jsx";
 
 export default function Pages() {
   return (
@@ -14,6 +17,9 @@ export default function Pages() {
         <Route path="/home" element={<App />} />
         {/* the route below automatically routes "/" to "/home" page */}
         <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/popular" element={<Popular />} />
+        <Route path="/series" element={<Series />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/home.jsx
@@ -0,0 +1,76 @@
+import { Link } from "react-router-dom";
+// import { MovieList1, MovieList2, MovieList3 } from "../components/MovieList";
+// import SearchBox from "../components/SearchBox";
+
+
+const Home = () => {
+  return (
+    <div className="pt-12">
+      <div className="hero relative w-full h-[40vh]">
+        <span className="absolute top-0 left-0 w-full bg-bg-transparent1"></span>
+        <div className="relative w-full">
+          <div className="w-full text-center font-bold space-y-3 md:pt-8">
+            <h1 className="text-3xl md:text-7xl pt-8 md:pt-0">Welcome!</h1>
+            <p className="text-xl md:text-4xl max-w-4xl mx-auto">
+              Discover and explore recent and trending Movies & TV Shows
+            </p>
+            Search
+            {/* <SearchBox /> */}
+          </div>
+        </div>
+      </div>
+      <div className="px-4 w-full h-full bg-bg-color pt-14 pb-4">
+        <div className="trending max-w-4xl mx-auto">
+          <div className="flex justify-between space-x-4 items-center mb-8">
+            <div className="relative">
+              <h2 className="relative text-2xl font-bold pb-1">
+                What&apos;s Trending
+              </h2>
+              <span className="line absolute w-full h-0.5 bottom-0"></span>
+            </div>
+            <Link to="/trending">
+              <button className="bg-heart-color px-4 rounded-full">
+                View all
+              </button>
+            </Link>
+          </div>
+          Trending
+        </div>
+        <div className="popular max-w-4xl mx-auto mt-8">
+          <div className="flex justify-between space-x-4 items-center mb-8">
+            <div className="relative">
+              <h2 className="relative text-2xl font-bold pb-1">
+                Popular Movies
+              </h2>
+              <span className="line absolute w-full h-0.5 bottom-0"></span>
+            </div>
+            <Link to="/popular">
+              <button className="bg-heart-color px-4 rounded-full">
+                View all
+              </button>
+            </Link>
+          </div>
+          Popular Movies
+        </div>
+        <div className="series max-w-4xl mx-auto mt-8">
+          <div className="flex justify-between space-x-4 items-center mb-8">
+            <div className="relative">
+              <h2 className="relative text-2xl font-bold pb-1">
+                Popular Series
+              </h2>
+              <span className="line absolute w-full h-0.5 bottom-0"></span>
+            </div>
+            <Link to="/series">
+              <button className="bg-heart-color px-4 rounded-full">
+                View all
+              </button>
+            </Link>
+          </div>
+          Popular Series
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

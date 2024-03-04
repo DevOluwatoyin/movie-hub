aeb1371164c68135b1a9fdb2e983277b3078d1b3
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2024-03-04 09:19:00 +0100

    Add heart icon to card component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,8 +7,22 @@ const MovieCard = ({ movie }) => {
       to={`/movies/${movie.id}`}
       className="line relative shadow-box-shadow pt-2 rounded-lg overflow-hidden hover:opacity-[0.6]"
     >
-      <p>icon</p>
-      {/* <HeartIcon className="absolute w-6 h-6 text-heart-outline right-0" /> */}
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="white"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="feather feather-heart favourite"
+        color="purple"
+      >
+        <g>
+          <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+        </g>
+      </svg>
       <img
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         alt={movie.title}

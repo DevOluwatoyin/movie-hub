a4fc3f2d33f2e3f78493b1a122e89ab1828e669d
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2024-03-05 15:08:13 +0100

    Modify navigation items styles

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
-const NavItem = ({items, onClick}) => {
+const NavItem = ({ items, onClick }) => {
+  const location = useLocation();
+
+  // Check if the current path matches the NavItem path
+  const isActive = location.pathname === items.path;
+
   return (
-    <li className="hover:underline hover:text-heart-color-hover">
+    <li
+      className={`hover:underline  underline-offset-4 transition-all ${
+        isActive
+          ? "text-red-500 underline hover:text-heart-color-hover"
+          : "hover:text-heart-color-hover"
+      }`}
+    >
       <Link className="lg:text-xl" to={items.path} onClick={onClick}>
         {items.name}
       </Link>
     </li>
   );
-}
+};
 
-export default NavItem
+export default NavItem;

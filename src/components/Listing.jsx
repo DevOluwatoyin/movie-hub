2a9dffed09940d641961cf0cbeb8561441e1f6b9
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2024-03-05 20:39:14 +0100

    Add styles for a  loader

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Listing = ({list}) => {
+const Listing = ({ list }) => {
   return (
     <div className="max-w-4xl mx-auto">
       <div className="flex justify-between space-x-4 items-center mb-8">
         <div className="relative">
-          <h2 className="relative text-2xl font-bold pb-1">
-            {list.title}
-          </h2>
+          <h2 className="relative text-2xl font-bold pb-1">{list.title}</h2>
           <span className="line absolute w-full h-0.5 bottom-0"></span>
         </div>
         <Link to={list.path}>
-          <button className="bg-heart-color py-2 px-8 rounded-full hover:bg-white hover:text-black transition-all">View all</button>
+          <button className="bg-heart-color py-2 px-8 rounded-full hover:bg-white hover:text-black transition-all">
+            View all
+          </button>
         </Link>
       </div>
       {list.component}
     </div>
   );
-}
+};
 
-export default Listing
+export default Listing;

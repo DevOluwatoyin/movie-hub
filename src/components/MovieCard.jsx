11af5fbbe30321bc2534aba2ff583c6584ad6330
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2024-03-05 02:38:49 +0100

    Enable add to favourites feature

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie }) => {
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    const isClicked = localStorage.getItem(`movie_${movie.id}_clicked`);
+    if (isClicked) {
+      setClicked(true);
+    }
+  }, [movie.id]);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setClicked(!clicked);
+    localStorage.setItem(`movie_${movie.id}_clicked`, !clicked);
+  };
+
   return (
     <Link
       to={`/movie/${movie.id}`}
@@ -11,13 +27,14 @@ const MovieCard = ({ movie }) => {
         width="24"
         height="24"
         viewBox="0 0 24 24"
-        fill="white"
+        fill={clicked ? "rgb(86, 42, 90)" : "white"}
         stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
         className="feather feather-heart favourite absolute right-5 top-5 z-10"
         color="purple"
+        onClick={handleClick}
       >
         <g>
           <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>

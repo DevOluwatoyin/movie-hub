61c384bf7e2610e2971be8539377304b0ede9fcd
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2024-03-05 09:48:51 +0100

    Improve Ui for 404 page

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      NotFound
+    <div className="h-screen grid place-items-center">
+      <div className="text-center text-xl md:text-2xl">
+        <p>I greet you boss! 🙌</p>
+        <p>
+          This page no dey. Abeg go👉
+          <Link to="/" className="text-[#ff2901] underline underline-offset-4 hover:text-white hover:tracking-widest transition-colors">
+            Home
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
